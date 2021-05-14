0ee84243bb188f68e851aae057357f36283b1328
Mary Gooneratne <mmg53@duke.edu>
2021-05-14 13:34:19 -0600

    hi

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import QueueAnim from 'rc-queue-anim';
+import React from "react";
+import PropTypes from "prop-types";
+import QueueAnim from "rc-queue-anim";
+import { Button } from "antd";
 // import { Button } from 'antd';
 // import { Element } from 'rc-scroll-anim';
 // import BannerImage from './BannerImage';
@@ -11,20 +12,20 @@ class Banner extends React.PureComponent {
     className: PropTypes.string,
     isMobile: PropTypes.bool,
     // navToShadow: PropTypes.func,
-  }
+  };
   static defaultProps = {
-    className: 'banner',
-  }
+    className: "banner",
+  };
   render() {
     // const { className, isMobile, navToShadow } = this.props;
     const { className, isMobile } = this.props;
     return (
-      // <Element component="section" className={`${className}-wrapper page`}
+      // <Element component='section' className={`${className}-wrapper page`}
       // onChange={navToShadow}>
       //   <div className={className}>
       //     {/* <div className={`${className}-img-wrapper`}>
       //       {isMobile ?
-      //         <img width="100%" src={`${assets}/image/home/intro-landscape-3a409.svg`} alt="" />
+      //         <img width='100%' src={`${assets}/image/home/intro-landscape-3a409.svg`} alt='' />
       //         :
       //         <BannerImage />}
       //     </div> */}
@@ -33,35 +34,53 @@ class Banner extends React.PureComponent {
       //       className={`${className}-text-wrapper`}
       //       delay={300}
       //     >
-      //       <img src="https://res.cloudinary.com/standard-crypto/image/upload/v1617483065/Vector_rpcjz8.svg" alt="Standard" style={{width: "70vw"}}/>
-      //       {/* <p className="main-info" key="p">Standard Crypto is
+      //       <img src='https://res.cloudinary.com/standard-crypto/image/upload/v1617483065/Vector_rpcjz8.svg' alt='Standard' style={{width: '70vw'}}/>
+      //       {/* <p className='main-info' key='p'>Standard Crypto is
       // developing decentralized financial services to give consumers the fastest,
       // cheapest and most diverse
       // suite of financial options in history.</p> */}
       //     </QueueAnim>
       //   </div>
       // </Element>
-      <div className={className}>
+      <div className={className} style={{}}>
         {/* <div className={`${className}-img-wrapper`}>
           {isMobile ?
-            <img width="100%" src={`${assets}/image/home/intro-landscape-3a409.svg`} alt="" />
+            <img width='100%' src={`${assets}/image/home/intro-landscape-3a409.svg`} alt='' />
             :
             <BannerImage />}
         </div> */}
-        <QueueAnim
-          type={isMobile ? 'bottom' : 'right'}
-          delay={300}
-        >
+        <QueueAnim type={isMobile ? "bottom" : "right"} delay={300}>
           <img
             src="https://res.cloudinary.com/standard-crypto/image/upload/v1617483065/Vector_rpcjz8.svg"
             alt="Standard"
             style={{
-            width: '70vw',
-            marginLeft: '15vw',
-            marginRight: '15vw',
-            marginTop: '40vh',
-                    }} />
-          {/* <p className="main-info" key="p">Standard Crypto is developing decentralized
+              width: "70vw",
+              marginLeft: "15vw",
+              marginRight: "15vw",
+              marginTop: "40vh",
+              marginBottom: "10vh",
+            }}
+          />
+          <Button
+            href="https://zampbaijbde.typeform.com/to/pOkNchL1"
+            style={{
+              border: "1px solid #6600E8",
+              color: "#6600E8",
+              alignContent: "center",
+              borderRadius: "5px",
+              width: "106px",
+              display: "flex",
+              margin: "auto",
+              fontSize: "18px",
+              paddingTop: "5px",
+              height: "44px",
+              fontFamily: ["-apple-system, BlinkMacSystemFont, sans-serif"],
+            }}
+          >
+            Join Beta
+          </Button>
+
+          {/* <p className='main-info' key='p'>Standard Crypto is developing decentralized
           financial services to give consumers the fastest,
           cheapest and most diverse suite of financial options in history.</p> */}
         </QueueAnim>
